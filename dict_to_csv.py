8bda64af211fbfa6c244cabd413afff09dfd6955
savilard <vitaliyantonoff@gmail.com>
2021-02-26 13:31:22 +0600

    feat: add solution of task "dict to csv"

--- dict_to_csv.py
+++ dict_to_csv.py
@@ -1,21 +1,37 @@
-"""
+import csv
+from typing import Dict, List
 
-Домашнее задание №2
 
-Работа csv
+def save_users_to_csv_file(users: List[Dict[str, str]]) -> None:
+    """Saves users data to csv file."""
+    with open('export.csv', 'w', encoding='utf-8') as csv_file:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(csv_file, fields, delimiter=';')
+        writer.writeheader()
+        for user in users:
+            writer.writerow(user)
 
-1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору. 
-   В списке нужно создать не менее 4-х словарей
-2. Запишите содержимое списка словарей в файл в формате csv
-
-"""
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    """Домашнее задание №2.
 
-if __name__ == "__main__":
+    Работа csv
+
+    1. Создайте список словарей с ключами name, age и job и значениями по вашему выбору.
+       В списке нужно создать не менее 4-х словарей
+    2. Запишите содержимое списка словарей в файл в формате csv
+
+    """
+    users = [
+        {'name': 'Алекс', 'age': '25', 'job': 'Инструктор по серфингу'},
+        {'name': 'Катерина', 'age': '34', 'job': 'Библиотекарь'},
+        {'name': 'Иван', 'age': '19', 'job': 'Студент'},
+        {'name': 'Пётр', 'age': '29', 'job': 'Фитнес тренер'},
+        {'name': 'Елена', 'age': '40', 'job': 'Бухгалтер'},
+    ]
+
+    save_users_to_csv_file(users)
+
+
+if __name__ == '__main__':
     main()
